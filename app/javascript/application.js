b4c939f31fb170c66c01a69c9f95254d6c4fc6d1
= <=>
2022-02-10 16:42:32 +0500

    email verification

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+const dropdown = document.querySelector('.dropdown')
+const dropdownBtn = document.querySelector('.dropdown-btn')
+dropdownBtn.addEventListener('click', e => {
+    e.preventDefault()
+    dropdown.classList.toggle('active')
+})
